bad434f11b8e400103942dfa4c8e187aea3e1ca9
Shawn Bahramy <shawnbahramy@gmail.com>
2018-02-09 14:15:27 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+import { EnergyComponent } from './energy/energy.component';
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  // { path: 'home', component: CrisisListComponent },
+  { path: 'energy',      component: EnergyComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnergyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+      //{ enableTracing: true } // <-- when debugging
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
